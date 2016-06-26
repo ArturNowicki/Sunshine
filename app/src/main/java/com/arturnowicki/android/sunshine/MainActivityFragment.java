8c313ccc528df5031a1e62feacde63c753b670e4
ArturN <arturnowicki@gmail.com>
2016-06-26 17:22:04 +0200

    finished lesson 1

--- app/src/main/java/com/arturnowicki/android/sunshine/MainActivityFragment.java
+++ app/src/main/java/com/arturnowicki/android/sunshine/MainActivityFragment.java
@@ -5,11 +5,19 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+
+    private ArrayList<String> forecastList;
+    private ArrayAdapter<String> mForecastAdapter;
+    private ListView weatherListView;
 
     public MainActivityFragment() {
     }
@@ -17,6 +25,15 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        forecastList.add(0, "Today - Sunny - 32/25");
+        forecastList.add(1, "Tomorrow - Sunny - 31/22");
+        forecastList.add(2, "Mon - Cloudy - 28/22");
+        forecastList.add(3, "Tue - Rainy - 27/20");
+        forecastList.add(4, "Wed - Foggy - 25/21");
+        mForecastAdapter = new ArrayAdapter<>(getActivity(), R.layout.list_item_forecast, R.id.listview_forecast, forecastList);
+        weatherListView = (ListView)rootView.findViewById(R.id.listview_forecast);
+        weatherListView.setAdapter(mForecastAdapter);
+        return rootView;
     }
 }
